a1bb03bd2b4c24f2d258bbbb1a532c421558ded5
daniskina1 <daniskina@neoflex.ru>
2025-10-01 16:47:52 +0300

    создание контактов (начальный вариант,перенос в базовый класс функций)

--- addressbook-web-tests/src/test/java/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/addressbook/ContactCreationTests.java
@@ -1,7 +1,6 @@
 package addressbook;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 public class ContactCreationTests extends TestBase {
 
@@ -14,32 +13,5 @@ public class ContactCreationTests extends TestBase {
             returnToContact();
         }
 
-    public void returnToContact() {
-        wd.findElement(By.linkText("home page")).click();
-    }
-
-    public void submitContactCreation() {
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    }
-
-    public void fillContactForm(ContactDate contactDate) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(contactDate.firstname());
-        wd.findElement(By.name("middlename")).click();
-        wd.findElement(By.name("middlename")).clear();
-        wd.findElement(By.name("middlename")).sendKeys(contactDate.middlename());
-        wd.findElement(By.name("mobile")).click();
-        wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys(contactDate.mobile());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(contactDate.email());
-    }
-
-    public void initContactCreation() {
-        wd.findElement(By.linkText("add new")).click();
-        wd.get("http://localhost/addressbook/edit.php");
-    }
 }
 
--- addressbook-web-tests/src/test/java/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/addressbook/TestBase.java
@@ -89,4 +89,32 @@ public class TestBase {
     protected void selectGroup() {
         wd.findElement(By.name("selected[]")).click();
     }
+
+    public void returnToContact() {
+        wd.findElement(By.linkText("home page")).click();
+    }
+
+    public void submitContactCreation() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    public void fillContactForm(ContactDate contactDate) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactDate.firstname());
+        wd.findElement(By.name("middlename")).click();
+        wd.findElement(By.name("middlename")).clear();
+        wd.findElement(By.name("middlename")).sendKeys(contactDate.middlename());
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys(contactDate.mobile());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(contactDate.email());
+    }
+
+    public void initContactCreation() {
+        wd.findElement(By.linkText("add new")).click();
+        wd.get("http://localhost/addressbook/edit.php");
+    }
 }
