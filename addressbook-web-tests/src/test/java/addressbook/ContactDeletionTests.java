a984aabfe3179af66a9989fc01e7aeb28dbb1e06
daniskina1 <daniskina@neoflex.ru>
2025-10-01 16:38:11 +0300

    удаление контактов (начальный вариант)

---
+++ addressbook-web-tests/src/test/java/addressbook/ContactDeletionTests.java
@@ -0,0 +1,76 @@
+package addressbook;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class ContactDeletionTests {
+    private WebDriver wd;
+    JavascriptExecutor js;
+    private boolean acceptNextAlert = true;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        wd = new ChromeDriver();
+        js = (JavascriptExecutor) wd;
+    }
+
+    @Test
+    public void testContactDeletion() throws Exception {
+        wd.get("http://localhost/addressbook/");
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.xpath("//input[@value='Login']")).click();
+        acceptNextAlert = true;
+        wd.findElement(By.id("5")).click();
+        wd.findElement(By.xpath("//input[@value='Delete']")).click();
+        assertTrue(closeAlertAndGetItsText().matches("^Delete 1 addresses[\\s\\S]$"));
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        wd.quit();
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = wd.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
