2217e6635655fa076db7501fcc356ad861038f60
daniskina1 <daniskina@neoflex.ru>
2025-10-01 14:47:24 +0300

    задание4 (удаление групп, создание общего базового класса)

--- addressbook-web-tests/src/test/java/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/addressbook/GroupCreationTests.java
@@ -1,37 +1,8 @@
 package addressbook;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.time.Duration;
-
-import static org.junit.jupiter.api.Assertions.fail;
-
-public class GroupCreationTests {
-    private WebDriver wb;
-    JavascriptExecutor js;
-
-    @BeforeEach
-    public void setUp()  {
-        wb = new ChromeDriver();
-        wb.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
-        js = (JavascriptExecutor) wb;
-        wb.get("http://localhost/addressbook/");
-        login("admin", "secret");
-    }
-
-    private void login(String username, String password) {
-        wb.findElement(By.name("user")).click();
-        wb.findElement(By.name("user")).clear();
-        wb.findElement(By.name("user")).sendKeys(username);
-        wb.findElement(By.name("pass")).click();
-        wb.findElement(By.name("pass")).clear();
-        wb.findElement(By.name("pass")).sendKeys(password);
-        wb.findElement(By.xpath("//input[@value='Login']")).click();
-    }
+public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation()  {
@@ -40,58 +11,6 @@ public class GroupCreationTests {
         fillGroupForm(new GroupDate("test1", "test2", "test3"));
         submitGroupCreation();
         returnToGroupPage();
-    }
-
-    private void returnToGroupPage() {
-        wb.findElement(By.linkText("group page")).click();
-    }
-
-    private void submitGroupCreation() {
-        wb.findElement(By.name("submit")).click();
-    }
-
-    private void fillGroupForm(GroupDate groupDate) {
-        wb.findElement(By.name("group_name")).click();
-        wb.findElement(By.name("group_name")).clear();
-        wb.findElement(By.name("group_name")).sendKeys(groupDate.name());
-        wb.findElement(By.name("group_header")).click();
-        wb.findElement(By.name("group_header")).clear();
-        wb.findElement(By.name("group_header")).sendKeys(groupDate.header());
-        wb.findElement(By.name("group_footer")).click();
-        wb.findElement(By.name("group_footer")).clear();
-        wb.findElement(By.name("group_footer")).sendKeys(groupDate.footer());
-    }
-
-    private void initGroupCreation() {
-        wb.findElement(By.name("new")).click();
-    }
-
-    private void gotoGroupPage() {
-        wb.findElement(By.linkText("groups")).click();
-    }
-
-    @AfterEach
-    public void tearDown()  {
-        wb.findElement(By.linkText("Logout")).click();
-        wb.quit();
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            wb.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            wb.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
     }
 
 }
