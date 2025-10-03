616e96df65eac6c6ff252f4ca5f11d6b7e05346a
daniskina1 <daniskina@neoflex.ru>
2025-10-03 18:05:28 +0300

    3.4 лекция, Задание 7

--- addressbook-web-tests/src/test/java/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/addressbook/appmanager/ApplicationManager.java
@@ -23,11 +23,11 @@ public class ApplicationManager {
     }
 
     public void init() {
-        if (browser == Browser.CHROME.browserName()){
+        if (browser.equals(Browser.CHROME.browserName())){
             wd = new ChromeDriver();
-        } else if (browser == Browser.FIREFOX.browserName()) {
+        } else if (browser.equals(Browser.FIREFOX.browserName())) {
             wd = new FirefoxDriver();
-        } else if (browser == Browser.IE.browserName()) {
+        } else if (browser.equals(Browser.IE.browserName())) {
             wd = new InternetExplorerDriver();
         }
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
