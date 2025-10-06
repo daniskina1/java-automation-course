59585f981d2f8307608e5a6eb867324a6ba32f2c
daniskina1 <daniskina@neoflex.ru>
2025-10-06 10:57:01 +0300

    Задание 8 ( модификация контакта контакта)

--- addressbook-web-tests/src/test/java/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/ContactModificationTests.java
@@ -8,6 +8,9 @@ public class ContactModificationTests extends TestBase {
     @Test
 
     public void testContactModification() {
+        if (! app.getContactHelper().isThereContact()) {
+            app.getContactHelper().createContact(new ContactDate("test1", "test2", "test3", "test4","test1"));
+        }
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillContactForm(new ContactDate("test1", "test2", "test3", "test4", null), false);
         app.getContactHelper().submitContactModification();
