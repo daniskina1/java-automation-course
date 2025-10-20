5fef76126f4a633627ad3e475057424e5978f5cf
daniskina1 <daniskina@neoflex.ru>
2025-10-20 13:27:24 +0300

    Задание 9(готово)

--- addressbook-web-tests/src/test/java/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/ContactModificationTests.java
@@ -4,6 +4,7 @@ import addressbook.model.ContactDate;
 import org.junit.jupiter.api.Test;
 import org.testng.Assert;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -17,21 +18,22 @@ public class ContactModificationTests extends TestBase {
             app.getContactHelper().createContact(new ContactDate("test1", "test2", "test3", "test4","test1"));
         }
         List<ContactDate> before = app.getContactHelper().getContactList();
-
         app.getContactHelper().selectContact();
         app.getContactHelper().initContactModification();
-
         ContactDate contact = new ContactDate("test1", "test2", "test3", "test4", null);
         app.getContactHelper().fillContactForm(contact, false);
         app.getContactHelper().submitContactModification();
         app.getContactHelper().returnToContact();
-
         List<ContactDate> after = app.getContactHelper().getContactList();
 
         before.remove(before.size() -1);
         before.add(contact);
+        Comparator<? super ContactDate> byName =
+                (c1, c2) -> c1.lastname().compareToIgnoreCase(c2.lastname());
+        before.sort(byName);
+        after.sort(byName);
+        Assert.assertEquals(before, after);
 
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
 
     }
