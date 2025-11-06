2595f50ef987d86aa7e0d50500dddc85add22f21
daniskina1 <daniskina@neoflex.ru>
2025-11-06 19:39:49 +0300

    5.7 кеширование

--- addressbook-web-tests/src/test/java/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/addressbook/appmanager/GroupHelper.java
@@ -13,16 +13,20 @@ import java.util.Set;
 
 public class GroupHelper extends HelperBase {
 
+    private Groups groupCache = null;
 
     public Groups all() {
-        Groups groups = new Groups();
+        if (groupCache != null) {
+            return  new Groups(groupCache);
+        }
+        groupCache = new Groups();
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for (WebElement element : elements) {
             String name = element.getText();
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-            groups.add(new GroupDate().withId(id).withName(name));
+            groupCache.add(new GroupDate().withId(id).withName(name));
         }
-        return groups;
+        return new Groups(groupCache);
     }
 
     public GroupHelper(WebDriver wd) {
@@ -67,6 +71,7 @@ public class GroupHelper extends HelperBase {
         initGroupCreation();
         fillGroupForm(group);
         submitGroupCreation();
+        groupCache = null;
         returnToGroupPage();
     }
 
@@ -75,6 +80,7 @@ public class GroupHelper extends HelperBase {
         initGroupModification();
         fillGroupForm(group);
         sudmitGroupModification();
+        groupCache = null;
         returnToGroupPage();
     }
 
@@ -89,6 +95,7 @@ public class GroupHelper extends HelperBase {
     public void delete(GroupDate group) {
         selectGroupById(group.getId());
         deleteSelectGroup();
+        groupCache = null;
         returnToGroupPage();
     }
 }
