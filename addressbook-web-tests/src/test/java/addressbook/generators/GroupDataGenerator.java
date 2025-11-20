d3d3d9e3b4c1bafa9340afde559801f14f4773dc
daniskina1 <daniskina@neoflex.ru>
2025-11-20 13:02:40 +0300

    6.3 анализ опций командной строки

--- addressbook-web-tests/src/test/java/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/addressbook/generators/GroupDataGenerator.java
@@ -1,6 +1,9 @@
 package addressbook.generators;
 
 import addressbook.model.GroupDate;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -11,15 +14,31 @@ import java.util.List;
 
 public class GroupDataGenerator {
 
-    public static void main (String [] args) throws IOException {
-        int count = Integer.parseInt(args [0]);
-        File file = new File(args [1]);
+    @Parameter(names = "-c", description = "Group count")
+    public int count;
 
-        List<GroupDate> groups = generateGroups(count);
-        save (groups, file);
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+    public static void main (String [] args) throws IOException {
+        GroupDataGenerator generatior = new GroupDataGenerator();
+        JCommander JCommander = new JCommander(generatior);
+        try {
+            JCommander.parse(args);
+        } catch (ParameterException ex) {
+            JCommander.usage();
+            return;
+        }
+        generatior.run();
     }
 
-    private static void save(List<GroupDate> groups, File file) throws IOException {
+    private void run() throws IOException {
+        List<GroupDate> groups = generateGroups(count);
+        save (groups, new File(file));
+
+    }
+
+    private void save(List<GroupDate> groups, File file) throws IOException {
 
         Writer writer = new FileWriter(file);
         for (GroupDate group : groups) {
@@ -28,7 +47,7 @@ public class GroupDataGenerator {
         writer.close();
     }
 
-    private static List<GroupDate> generateGroups(int count) {
+    private List<GroupDate> generateGroups(int count) {
         List<GroupDate> groups = new ArrayList<GroupDate>();
         for (int i = 0; i < count; i++) {
             groups.add(new GroupDate().withName(String.format("test %s", i))
