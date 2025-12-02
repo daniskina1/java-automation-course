eb31df8a2775d0bffbb33db27c170ede4e9578e9
daniskina1 <daniskina@neoflex.ru>
2025-12-02 14:16:35 +0300

    Задание 15 создание групп

--- addressbook-web-tests/src/test/java/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/GroupCreationTests.java
@@ -63,10 +63,10 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation( GroupDate group)  {
         logger.info("Start test testGroupCreation");
         app.goTo().GroupPage();
-        Groups before = app.group().all();
+        Groups before = app.db().groups();
         app.group().create(group);
         assertThat(app.group().count(), equalTo (before.size() + 1));
-        Groups after = app.group().all();
+        Groups after = app.db().groups();
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
         logger.info("Stop test testGroupCreation");
